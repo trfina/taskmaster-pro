51368845ea7abd7760a5df7c0640060a694662bf
Tammy Fina <finatammy@gmail.com>
2021-11-22 15:36:44 -0500

    edit tasks complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,62 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// insert code here
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+  console.log(this);
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  $(this).replaceWith(textInput); 
+  textInput.trigger("focus"); 
+});  
 
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+var text = $(this)
+  .val()
+  .trim();
+
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+tasks[status][index].text=text;
+saveTasks();
+// recreate p element
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+});
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+}); 
 
 
 // modal was triggered
